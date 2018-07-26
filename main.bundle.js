85cd9554ee94c6c81c0cbffd0de4db5d25066c12
Aaqib Sheikh <aaqibnadeem123@gmail.com>
2018-07-27 00:11:47 +0500

    Event Calendar Task

---
+++ main.bundle.js
@@ -0,0 +1,359 @@
+webpackJsonp([1,4],{
+
+/***/ 303:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__mocks_events__ = __webpack_require__(460);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_rxjs_Observable__ = __webpack_require__(22);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_rxjs_Observable___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_2_rxjs_Observable__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3_rxjs_add_observable_of__ = __webpack_require__(622);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3_rxjs_add_observable_of___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_3_rxjs_add_observable_of__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return EventService; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+
+
+
+var EventService = (function () {
+    function EventService() {
+    }
+    EventService.prototype.getEvents = function () {
+        var _this = this;
+        return __WEBPACK_IMPORTED_MODULE_2_rxjs_Observable__["Observable"].of(__WEBPACK_IMPORTED_MODULE_1__mocks_events__["a" /* EVENT_LIST */].filter(function (event) { return _this.event = event; }));
+    };
+    EventService = __decorate([
+        __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["Injectable"])(), 
+        __metadata('design:paramtypes', [])
+    ], EventService);
+    return EventService;
+}());
+//# sourceMappingURL=C:/Users/AAQIB/Desktop/smartfix/smartcalendar/src/event.service.js.map
+
+/***/ }),
+
+/***/ 348:
+/***/ (function(module, exports) {
+
+function webpackEmptyContext(req) {
+	throw new Error("Cannot find module '" + req + "'.");
+}
+webpackEmptyContext.keys = function() { return []; };
+webpackEmptyContext.resolve = webpackEmptyContext;
+module.exports = webpackEmptyContext;
+webpackEmptyContext.id = 348;
+
+
+/***/ }),
+
+/***/ 349:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser_dynamic__ = __webpack_require__(436);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_core__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_environments_environment__ = __webpack_require__(461);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3_app_app_module__ = __webpack_require__(457);
+
+
+
+
+if (__WEBPACK_IMPORTED_MODULE_2_environments_environment__["a" /* environment */].production) {
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_1__angular_core__["enableProdMode"])();
+}
+__webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_platform_browser_dynamic__["a" /* platformBrowserDynamic */])().bootstrapModule(__WEBPACK_IMPORTED_MODULE_3_app_app_module__["a" /* AppModule */]);
+//# sourceMappingURL=C:/Users/AAQIB/Desktop/smartfix/smartcalendar/src/main.js.map
+
+/***/ }),
+
+/***/ 456:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppComponent; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+var AppComponent = (function () {
+    function AppComponent() {
+    }
+    AppComponent = __decorate([
+        __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["Component"])({
+            selector: 'app-root',
+            template: __webpack_require__(619),
+            styles: [__webpack_require__(617)]
+        }), 
+        __metadata('design:paramtypes', [])
+    ], AppComponent);
+    return AppComponent;
+}());
+//# sourceMappingURL=C:/Users/AAQIB/Desktop/smartfix/smartcalendar/src/app.component.js.map
+
+/***/ }),
+
+/***/ 457:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__ = __webpack_require__(136);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_core__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__angular_forms__ = __webpack_require__(279);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__angular_http__ = __webpack_require__(432);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4_daypilot_pro_angular__ = __webpack_require__(148);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4_daypilot_pro_angular___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_4_daypilot_pro_angular__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5__app_component__ = __webpack_require__(456);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_6__calendar_calendar_module__ = __webpack_require__(459);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_7__providers_event_service__ = __webpack_require__(303);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppModule; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+
+
+
+
+
+
+
+var AppModule = (function () {
+    function AppModule() {
+    }
+    AppModule = __decorate([
+        __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_1__angular_core__["NgModule"])({
+            declarations: [
+                __WEBPACK_IMPORTED_MODULE_5__app_component__["a" /* AppComponent */],
+            ],
+            imports: [
+                __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__["a" /* BrowserModule */],
+                __WEBPACK_IMPORTED_MODULE_2__angular_forms__["a" /* FormsModule */],
+                __WEBPACK_IMPORTED_MODULE_3__angular_http__["a" /* HttpModule */],
+                __WEBPACK_IMPORTED_MODULE_4_daypilot_pro_angular__["DayPilotModule"],
+                __WEBPACK_IMPORTED_MODULE_6__calendar_calendar_module__["a" /* CalendarModule */]
+            ],
+            providers: [__WEBPACK_IMPORTED_MODULE_7__providers_event_service__["a" /* EventService */]],
+            bootstrap: [__WEBPACK_IMPORTED_MODULE_5__app_component__["a" /* AppComponent */]]
+        }), 
+        __metadata('design:paramtypes', [])
+    ], AppModule);
+    return AppModule;
+}());
+//# sourceMappingURL=C:/Users/AAQIB/Desktop/smartfix/smartcalendar/src/app.module.js.map
+
+/***/ }),
+
+/***/ 458:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_daypilot_pro_angular__ = __webpack_require__(148);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_daypilot_pro_angular___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_1_daypilot_pro_angular__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__providers_event_service__ = __webpack_require__(303);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return CalendarComponent; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+
+
+var CalendarComponent = (function () {
+    function CalendarComponent(es) {
+        this.es = es;
+        // Sidebar Calendar View Type
+        this.navigatorConfig = {
+            selectMode: "week"
+        };
+        // Event Calendar Configuration for today date and calendar type
+        this.calendarConfig = {
+            startDate: __WEBPACK_IMPORTED_MODULE_1_daypilot_pro_angular__["DayPilot"].Date.today(),
+            viewType: "Week"
+        };
+    }
+    CalendarComponent.prototype.ngOnInit = function () {
+        // Getting Events Call
+        this.getEvents();
+    };
+    // Method for getting all events
+    CalendarComponent.prototype.getEvents = function () {
+        var _this = this;
+        this.es.getEvents().subscribe(function (data) { return _this.events = data; });
+    };
+    __decorate([
+        __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["ViewChild"])('calendar'), 
+        __metadata('design:type', (typeof (_a = typeof __WEBPACK_IMPORTED_MODULE_1_daypilot_pro_angular__["DayPilotCalendarComponent"] !== 'undefined' && __WEBPACK_IMPORTED_MODULE_1_daypilot_pro_angular__["DayPilotCalendarComponent"]) === 'function' && _a) || Object)
+    ], CalendarComponent.prototype, "calendar", void 0);
+    CalendarComponent = __decorate([
+        __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["Component"])({
+            selector: 'calendar',
+            template: __webpack_require__(620),
+            styles: [__webpack_require__(618)]
+        }), 
+        __metadata('design:paramtypes', [(typeof (_b = typeof __WEBPACK_IMPORTED_MODULE_2__providers_event_service__["a" /* EventService */] !== 'undefined' && __WEBPACK_IMPORTED_MODULE_2__providers_event_service__["a" /* EventService */]) === 'function' && _b) || Object])
+    ], CalendarComponent);
+    return CalendarComponent;
+    var _a, _b;
+}());
+//# sourceMappingURL=C:/Users/AAQIB/Desktop/smartfix/smartcalendar/src/calendar.component.js.map
+
+/***/ }),
+
+/***/ 459:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_forms__ = __webpack_require__(279);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_platform_browser__ = __webpack_require__(136);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__angular_core__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3_daypilot_pro_angular__ = __webpack_require__(148);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3_daypilot_pro_angular___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_3_daypilot_pro_angular__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__calendar_component__ = __webpack_require__(458);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return CalendarModule; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+
+
+
+
+var CalendarModule = (function () {
+    function CalendarModule() {
+    }
+    CalendarModule = __decorate([
+        __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_2__angular_core__["NgModule"])({
+            imports: [
+                __WEBPACK_IMPORTED_MODULE_1__angular_platform_browser__["a" /* BrowserModule */],
+                __WEBPACK_IMPORTED_MODULE_0__angular_forms__["a" /* FormsModule */],
+                __WEBPACK_IMPORTED_MODULE_3_daypilot_pro_angular__["DayPilotModule"],
+                __WEBPACK_IMPORTED_MODULE_0__angular_forms__["b" /* ReactiveFormsModule */]
+            ],
+            declarations: [
+                __WEBPACK_IMPORTED_MODULE_4__calendar_component__["a" /* CalendarComponent */]
+            ],
+            exports: [__WEBPACK_IMPORTED_MODULE_4__calendar_component__["a" /* CalendarComponent */]],
+            providers: []
+        }), 
+        __metadata('design:paramtypes', [])
+    ], CalendarModule);
+    return CalendarModule;
+}());
+//# sourceMappingURL=C:/Users/AAQIB/Desktop/smartfix/smartcalendar/src/calendar.module.js.map
+
+/***/ }),
+
+/***/ 460:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return EVENT_LIST; });
+// JSON data 
+var eventList = [
+    { id: 1, start: "2018-07-23T10:00:00", end: "2018-07-23T11:00:00", text: "Event 1", backColor: "#41f450" },
+    { id: 2, start: "2018-07-24T12:00:00", end: "2018-07-24T14:00:00", text: "Event 2", backColor: "#0bc49f" },
+    { id: 3, start: "2018-07-25T16:00:00", end: "2018-07-25T18:00:00", text: "Event 3", backColor: "#c13b32" },
+    { id: 4, start: "2018-07-26T20:00:00", end: "2018-07-26T24:00:00", text: "Event 4", backColor: "#42d83a" },
+    { id: 5, start: "2018-07-27T08:00:00", end: "2018-07-27T10:00:00", text: "Event 5", backColor: "#d65ea4" },
+    { id: 6, start: "2018-07-28T11:00:00", end: "2018-07-28T12:00:00", text: "Event 6", backColor: "#c13b32" },
+    { id: 7, start: "2018-07-29T13:00:00", end: "2018-07-29T14:00:00", text: "Event 7", backColor: "##5f6b77" },
+    { id: 8, start: "2018-07-30T08:00:00", end: "2018-07-30T11:00:00", text: "Event 8", backColor: "#4872a0" },
+    { id: 9, start: "2018-07-31T11:00:00", end: "2018-07-31T13:00:00", text: "Event 9", backColor: "#41f450" },
+    { id: 10, start: "2018-08-01T09:00:00", end: "2018-08-01T11:00:00", text: "Event 10", backColor: "#49936d" },
+    { id: 11, start: "2018-08-02T10:00:00", end: "2018-08-02T12:00:00", text: "Event 11", backColor: "#41f497" },
+    { id: 12, start: "2018-08-03T08:00:00", end: "2018-08-03T10:00:00", text: "Event 12", backColor: "#f4c441" },
+];
+var EVENT_LIST = eventList;
+//# sourceMappingURL=C:/Users/AAQIB/Desktop/smartfix/smartcalendar/src/events.js.map
+
+/***/ }),
+
+/***/ 461:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return environment; });
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `angular-cli.json`.
+var environment = {
+    production: false
+};
+//# sourceMappingURL=C:/Users/AAQIB/Desktop/smartfix/smartcalendar/src/environment.js.map
+
+/***/ }),
+
+/***/ 617:
+/***/ (function(module, exports) {
+
+module.exports = ""
+
+/***/ }),
+
+/***/ 618:
+/***/ (function(module, exports) {
+
+module.exports = ""
+
+/***/ }),
+
+/***/ 619:
+/***/ (function(module, exports) {
+
+module.exports = "\r\n<!-- Calendar Component Selector -->\r\n<calendar></calendar>"
+
+/***/ }),
+
+/***/ 620:
+/***/ (function(module, exports) {
+
+module.exports = "<div class=\"container\">\n\n\t<div class=\"row\">\n\t\t<h2 class=\"text-center\">Smart Calendar</h2>\n\t</div>\n\n\t<div class=\"row\">\n\t\t<div class=\"col-md-2\">\n\t\t\t<!-- Sidebar Calendar -->\n\t\t\t<daypilot-navigator [config]=\"navigatorConfig\" [(date)]=\"calendarConfig.startDate\"></daypilot-navigator>\n\t\t</div>\n\n\t\t<div class=\"col-md-9\">\n\t\t\t<!-- Event Calendar -->\n\t\t\t<daypilot-calendar [config]=\"calendarConfig\" #calendar [events]=\"events\"></daypilot-calendar>\t\t\t\n\t\t</div>\n\t</div>\n</div>\n\n"
+
+/***/ }),
+
+/***/ 644:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(349);
+
+
+/***/ })
+
+},[644]);
+//# sourceMappingURL=main.bundle.map
